f12ce29d3870bf4560b010df66788240cb56bee0
Kevin Tsai <k.tsai92@gmail.com>
2019-06-18 16:01:37 -0400

    Added React code

--- src/composition/Tooltip.js
+++ src/composition/Tooltip.js
@@ -17,4 +17,25 @@ function Tooltip(props) {
   )
 }
 
-export default Tooltip;
+class TooltipClass extends React.Component {
+	render() {
+		console.log('Using a class component!')
+		console.log(this.props)
+		return (
+			<span className= 'Tooltip'>
+				<span 
+					className= 'Tooltip-content'
+					style= {{ color: this.props.color}}
+				>
+					{this.props.children}
+				</span>
+				<div className ='Tooltip-message'>
+					{this.props.message}
+				</div>
+			</span>
+		)
+	}
+}
+
+
+export default TooltipClass;
